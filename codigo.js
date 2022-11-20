f17316400c53c6a91d3486f6b1f9cbdc1fdcf21d
ElisabethDB <dombaleli@gmail.com>
2022-11-20 09:47:05 +0100

    ahoraCreoQueEstaBien

--- codigo.js
+++ codigo.js
@@ -6,10 +6,26 @@ window.onload = el_tiempo;
 let elTiempo = document.getElementById('tiempo');
 let app = document.getElementById('app');
 let emojis = document.getElementById('contain_emojis');
+let btnEmpezar = document.getElementById('empezar');
 let btnChiste = document.getElementById('boton1');
 let pregunta = document.getElementById('pregunta');
 let reportJokes = [];
 let puntos, joke;
+
+let nuevoChiste = () =>{
+ 
+  const url='https://icanhazdadjoke.com//slack';
+  const peticion=fetch(url)
+    peticion.then(datos => datos.json())
+            .then(data =>  {
+              emojis.style.display = 'block';
+              btnChiste.style.display = 'none';
+              pregunta.style.display ='block';
+              app.innerHTML = data.attachments[0].text;
+              joke = data.attachments[0].text;             
+            })
+            .catch(() => alert("error"))                                    
+}  
 
 let fecha = () => {
   const d = new Date();
@@ -18,47 +34,35 @@ let fecha = () => {
 } 
 
 let score = (num) =>{
-  puntos = num;
+  if(num == 1){
+    puntos = 1;
+  }else if(num == 2){
+    puntos = 2;
+  }else if(num == 3){
+    puntos = 3;
+  }
   btnChiste.style.display = 'block';
   pregunta.style.display ='none';
+
   llenarArray();
 }
-
-let nuevoChiste = () =>{
- 
-    const url='https://icanhazdadjoke.com//slack';
-    const peticion=fetch(url)
-      peticion.then(datos => datos.json())
-              .then(data =>  {
-                emojis.style.display = 'block';
-                btnChiste.style.display = 'none';
-                pregunta.style.display ='block';
-                app.innerHTML = data.attachments[0].text;
-                joke = data.attachments[0].text;  
-                //imgBlob.style.background = `${url_img}`;               
-              })
-              .catch(() => alert("error"))                            
-}  
-/*let segundoChiste = () =>{
-
-  const url='https://api.chucknorris.io/jokes/random';
-  const peticion2=fetch(url)
-    peticion2.then(dades => dades.json())
-            .then(dato =>  {
-              app.innerHTML = dato.value;
-              joke1 = dato.value;
-              //imgBlob.style.background = `${url_img}`;
-            })
-            .catch(() => alert("error"))                     
-}*/      
+     
 
 let llenarArray = () => {
+  
+  let opinion = new Opinions(joke, puntos, fecha());
+  reportJokes.push(opinion);
 
-    let opinion = new Opinions(joke, puntos, fecha());
-    reportJokes.push(opinion);
- 
+  for( let j of reportJokes){
+    let l = reportJokes.length;
+    if(reportJokes[l-1].joke == reportJokes[l-2].joke){
+    reportJokes.splice([l-2], 1);
+    }
+  }
 } 
-console.log(reportJokes);                           
+
+console.log(reportJokes);
+
 
 function el_tiempo(){
   const url='https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/08075';
@@ -70,6 +74,7 @@ function el_tiempo(){
             .catch(() => alert("error"))          
 }
 
+document.getElementById('boton1').onclick = nuevoChiste;
 
 
 
